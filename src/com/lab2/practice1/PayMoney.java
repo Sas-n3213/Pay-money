595c4aa1aa526c8e41ac9c3d4ead8d0fcada9d59
Ankit srivastava <99071276+Sas-n3213@users.noreply.github.com>
2022-04-11 08:19:28 +0530

    Add files via upload

---
+++ src/com/lab2/practice1/PayMoney.java
@@ -0,0 +1,53 @@
+package com.lab2.practice1;
+
+import java.util.Scanner;
+
+public class PayMoney {
+
+	public static void main (String[] args) {
+		
+		Scanner sc= new Scanner (System.in);
+		System.out.println("Enter the size of array:");
+		int size = sc.nextInt();
+		int arr [] = new int [size];
+		System.out.println("Enter the elements of array:");
+		
+		for (int i =0; i<size; i++) {
+			arr[i]=sc.nextInt();
+		}
+System.out.println("Enter the total no of targets that needs to be achieved:");
+
+         int t =sc.nextInt();
+		for (int i =1; i<=t; i++) {
+			System.out.println("Enter the value of target:");
+			int target =sc.nextInt();
+		
+			int sum=0;
+			for (int j =0; j<size; j++) {
+				
+				sum = sum + arr[j];
+			
+
+				if (sum >= target) {
+					System.out.println("Target achieved after"  +(j+1)+   "transactions");
+					break;
+				}
+			}
+				if(sum<target) {
+					System.out.println("Target cannot be achieved");
+				}
+					
+			}
+			
+		
+		
+		
+		
+	
+		
+		
+	
+		
+}
+
+}
