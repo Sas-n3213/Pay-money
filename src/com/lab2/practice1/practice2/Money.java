c13f4295ac5a50eb04ca0f165e0951b55d11d28e
Ankit srivastava <99071276+Sas-n3213@users.noreply.github.com>
2022-04-11 21:34:09 +0530

    Update Money.java

--- src/com/lab2/practice1/practice2/Money.java
+++ src/com/lab2/practice1/practice2/Money.java
@@ -10,7 +10,7 @@ public static void main (String args[]) {
 	int[] notes=new int [size];
 	System.out.println("Enter the currency denomination value:");
 	for(int i=0; i<size; i++) {
-		notes[i]=sc.nextInt();
+	notes[i]=sc.nextInt();
 		
 	}
 	System.out.println("Enter the amount:");
@@ -18,17 +18,17 @@ public static void main (String args[]) {
 	Money bs = new Money();
 	bs.sort(notes);
 	Money notesCount = new Money();
-    notesCount.notesCountImplementation(notes,amount);
+        notesCount.notesCountImplementation(notes,amount);
 }
 
 public void sort (int[]notes) {
 	int n= notes.length;
 	for (int i=0; i<n-1 ; i++) {
-		for (int j=0; j<n-i-1 ; j++) {
-			if (notes[j]<notes[j+1]) {
-				int temp=notes[j];
-				notes[j]=notes[j+1];
-				notes[j+1]=temp;
+	for (int j=0; j<n-i-1 ; j++) {
+	if (notes[j]<notes[j+1]) {
+	int temp=notes[j];
+	notes[j]=notes[j+1];
+	notes[j+1]=temp;
 			}
 		}
 	}
@@ -37,9 +37,9 @@ public void sort (int[]notes) {
 public void notesCountImplementation(int notes[],int amount) {
 	int[] noteCounter = new int[notes.length];
 	for (int i=0; i<notes.length; i++) {
-		if (amount>=notes[i]) {
-			noteCounter[i]= amount/notes[i];
-			amount=amount-noteCounter[i]*notes[i];
+	if (amount>=notes[i]) {
+	noteCounter[i]= amount/notes[i];
+	amount=amount-noteCounter[i]*notes[i];
 		}
 	}
 	if (amount>0)
@@ -49,8 +49,8 @@ public void notesCountImplementation(int notes[],int amount) {
 	else {
 		System.out.println("Your payment approach in order to give min no of notes will be ");
 		for (int i=0; i<notes.length;i++) {
-			if (noteCounter[i]!=0) {
-				System.out.println(notes[i]  +  ":"   + noteCounter[i]);
+		if (noteCounter[i]!=0) {
+		System.out.println(notes[i]  +  ":"   + noteCounter[i]);
 			}
 		
 			
